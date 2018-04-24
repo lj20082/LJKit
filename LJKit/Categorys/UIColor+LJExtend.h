67e418f99af1c7f0be95ce2f4f059f51ad643ce6
673637753@qq.com <673637753@qq.com>
2018-04-24 15:52:18 +0800

    添加UIColor扩展

---
+++ LJKit/Categorys/UIColor+LJExtend.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+LJExtend.h
+//  LJKitDemo
+//
+//  Created by 李建忠 on 2018/4/24.
+//  Copyright © 2018年 IFlytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (LJExtend)
+
+- (CGFloat)lj_alpha;
+
+- (nullable NSString *)lj_hexString;
+
+@end
